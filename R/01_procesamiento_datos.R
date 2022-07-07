05f342c7a2e5dbbde513b2085def903f825e166a
lebalcazar <lebalcazar@gmail.com>
2022-07-06 23:32:09 -0500

    actualizacion con procesamiento de datos PERSIANN año 2021

--- R/01_procesamiento_datos.R
+++ R/01_procesamiento_datos.R
@@ -1,12 +1,73 @@
 # procesar datos de pronóstico 
 library(tidyverse)
 library(lubridate)
+library(fitdistrplus)
+library(terra)
+library(sf)
+library(raster)
 
 # leer datos de pronostico desde proyeto lbo_doc:
 # "/home/luisbalcazar/Documentos/lbo_doc/resultados/datos_preProceso/datos_f.rds"
 
+# datos sst junio-2020 a octubre 2021 
+# da <- read_rds("../lbo_doc/resultados/datos_preProceso/datos_f.rds")
+# sst_2020 <- da %>% 
+#   dplyr::filter(date >= "2020-01-01") %>% 
+#   dplyr::filter(reg_sst %in% c("s1", "s2", "s3"), reg_mslp == "m2", reg_rhum == "r3") %>% 
+#   dplyr::select(date, sst, reg_sst) %>% 
+#   dplyr::distinct(date, reg_sst, .keep_all = TRUE)
+  
+
+
 # datos 1983-2020
-da <- read_rds("../lbo_doc/resultados/datos_preProceso/datos_f.rds")
+# datos_pron <- read_rds("../lbo_doc/resultados/pronostico_prc_rds_por_pixel/pronostico_poly_mensual.rds")
+
+# datos PERSIANN 2021 
+lf <- list.files("/media/luis/644bedd8-fbc1-476d-9939-a613c80145d9/luisbalcazar/Documentos/04SIG/01Rawdata/Prc/cdr_global/mes/PERSIANN-CDR_mensual_global_Tif/CDR_2022-07-04033154pm", 
+                 pattern = ".tif$", full.names = TRUE, all.files = TRUE)
+rst_21 <- raster::stack(lf)
+
+cnc <- readRDS("../lbo_doc/datos/sig/raster/mask_cnc_est.rds")
+
+rst_crop <- raster::crop(rst_21, cnc)
+
+rst_msk <- raster::mask(rst_crop, cnc)
+
+
+writeRaster(x = rst_msk, 
+            filename = names(rst_msk), bylayer = T, format = "tif")
+
+writeRaster(x = rst_msk, 
+            filename = paste0("/media/luis/644bedd8-fbc1-476d-9939-a613c80145d9/luisbalcazar/Documentos/04SIG/01Rawdata/Prc/cdr_AfrOcc/mes/CDR_AfrOcc_cnc_est/cdr_2021/", 
+                              names(rst_msk),
+                              ".tif"), 
+            bylayer = T)
+
+# estaciones 
+coorEstCnc <- readRDS('/media/luis/644bedd8-fbc1-476d-9939-a613c80145d9/luisbalcazar/Documentos/sahel/data/rds/coorEstCnc.rds') %>% as_tibble()
+            
+cdr2021_tbl <- rasterToPoints(rst_msk, xyFromCell = TRUE) %>% as_tibble()
+
+# datos y coordenadas
+dat <- left_join(coorEstCnc, cdr2021_tbl, by = c("x", "y"))
+
+# tabla de datos cdr diarios 
+tbl <- dat %>% 
+  pivot_longer(cols = -c(x, y, name), names_to = "date", values_to = "cdr") %>% 
+  mutate(date = as.Date(paste(date, 01), "CDR_%Y%m%d"), 
+         id = paste(x, y, name, sep = "_")) %>% 
+  # group_by(id, year = year(date), month = month(date)) %>% 
+  # mutate(cdr = ifelse(is.na(cdr), 
+  #                     rnorm(n = ., mean(cdr, na.rm = TRUE)), 
+  #                     cdr)) %>% 
+  mutate(cdr = ifelse(cdr < 1, 0, cdr)) %>% 
+  mutate(cdr = ifelse(is.na(cdr), 0, cdr)) %>% 
+  ungroup() %>% 
+  dplyr::select(x, y, name, date, cdr)
+
+# guardar
+saveRDS(tbl, "/media/luis/644bedd8-fbc1-476d-9939-a613c80145d9/luisbalcazar/Documentos/sahel/data/rds/cdr2021_por_pixel.rds")
+
 
 # ecuaciones
 # lluvia de 2021-2022 coeficientes para caada pixel -> tener lluvia mensual may-oct
