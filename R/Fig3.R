2bbde99d79b490ba15ad0415a9f69965546c25a2
gavg712 <gavg712@gmail.com>
2022-10-16 16:56:50 -0500

    modificaciones de estilo en figura 3

--- R/Fig3.R
+++ R/Fig3.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(sf)
+library(extrafont)
+#extrafont::ttf_import("data/ttf/Palatino Linotype.ttf")
 
 # data of output models
 performance = read_csv("outputs/models/model_performance.csv")
@@ -20,26 +22,31 @@ performance_best <- performance %>%
   ) 
 
 # polygons of basin and rain gauge
-basin <- st_read('data/gpkg/vector_layers.gpkg', layer = "basin")  
-rain_gauges <- st_read("data/gpkg/vector_layers.gpkg", layer = "ubc_est20")
-
+basin <- st_read('data/gpkg/vector_layers.gpkg', layer = "river basins") 
+rain_gauges <- st_read("data/gpkg/vector_layers.gpkg", layer = "rain gauges")
+pal <- c("s1: poly" = "#ffffb3", "s3: poly" = "#fdb462", "s3: step" = "#fb8072")
+ 
 # Which region delivers the best performance?
 ggplot() +
   geom_tile(data = performance_best, 
             aes(x, y, fill = which)) +
   geom_sf(data = basin, fill = NA) +
   geom_sf(data = rain_gauges) +
-  geom_text(data = rain_gauges, aes(x = x, y = y, label = name), 
-            # position = position_dodge(width = 1),
-            vjust = -0.2) +
-  scale_fill_brewer(palette = "Dark2",
-                    labels = c("SST1: polynomial", "SST3: polynomial", "SST3: stepwise")) +
-  labs(x = "Longitude", y = "Latitude", fill = "Region: model") + 
-  theme_bw(base_size = 10) +
-  theme(legend.position = c(.82,.86),        
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank()) 
+  geom_text(data = rain_gauges, size = 3,
+           aes(x = x, y = y, label = name), 
+           nudge_y = c(rep(0.25, 12), -0.25, rep(0.25, 7))) +
+ scale_fill_manual(values = pal,
+                   labels = c("SST1: polynomial", "SST3: polynomial", "SST3: stepwise")) +
+ labs(x = "Longitude", y = "Latitude") +
+ theme_classic(base_size = 10, base_family = "Palatino Linotype") +
+ theme(panel.grid.major = element_blank(), 
+       panel.grid.minor = element_blank(), 
+       legend.position = c(0.99, 0.99),
+       legend.justification = c(1, 1),
+       legend.title = element_blank(), 
+       legend.key.size = unit(0.04, "npc"))
 
 # save last plot
-ggsave(filename = 'outputs/plots/fig3.png', plot = last_plot(), device = 'png', 
-      units = 'cm', height = 18, width = 14, dpi = 300)
+ggsave(filename = "Fig3.png", path = 'outputs/plots/', device = 'png', 
+       units = 'cm', height = 10, width = 10, 
+       dpi = 300, scale = 1.25)
